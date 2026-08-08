3cd2ee93aff87ee31dfeb12a0564da3a691b3344
baonguyen5759 <quocnb@gmail.com>
2026-08-07 19:56:48 -0500

    add stream to Map

--- src/main/java/com/bao/core_java/stream/TopStudentBySubject.java
+++ src/main/java/com/bao/core_java/stream/TopStudentBySubject.java
@@ -9,17 +9,21 @@ import java.util.stream.Collectors;
 
 @Slf4j
 public class TopStudentBySubject {
-
+    /*
     @Data
     @AllArgsConstructor
     class Student {
         private String studentName;
         private String subject;
         private int score;
-    }
+    }*/
+
+    record Student (String studentName, String subject, int score) {};
 
     void main() {
+
         List<Student> students = new ArrayList<>();
+
         students.add(new Student("AA", "Math", 90));
         students.add(new Student("BB", "Math", 85));
         students.add(new Student("CC", "Math", 70));
@@ -28,24 +32,24 @@ public class TopStudentBySubject {
         students.add(new Student("FF", "Physics", 98));
 
         // Method 1
-        Map<String, List<Student>> mapStudentBySubject = students.stream().collect(Collectors.groupingBy(Student::getSubject));
+        Map<String, List<Student>> mapStudentBySubject = students.stream().collect(Collectors.groupingBy(Student::subject));
         mapStudentBySubject.entrySet().forEach(x -> {
-            Optional<Student> optStudent = x.getValue().stream().max(Comparator.comparingInt(Student::getScore));
+            Optional<Student> optStudent = x.getValue().stream().max(Comparator.comparingInt(Student::score));
             if (optStudent.isPresent()) {
-                log.info("Method 1: Subject: {} | Student: {} | Score: {}", x.getKey(), optStudent.get().getStudentName(), optStudent.get().getScore());
+                log.info("Method 1: Subject: {} | Student: {} | Score: {}", x.getKey(), optStudent.get().studentName(), optStudent.get().score());
             }
         });
 
         // Method 2:
-        Map<String, Student> mapTopStudent = students.stream().collect(Collectors.groupingBy(Student::getSubject, Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparingInt(Student::getScore)), Optional::get)));
+        Map<String, Student> mapTopStudent = students.stream().collect(Collectors.groupingBy(Student::subject, Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparingInt(Student::score)), Optional::get)));
         mapTopStudent.entrySet().forEach(x -> {
-            log.info("Method 2: Subject: {} | Student: {} | Score: {}", x.getKey(), x.getValue().getStudentName(), x.getValue().getScore());
+            log.info("Method 2: Subject: {} | Student: {} | Score: {}", x.getKey(), x.getValue().studentName(), x.getValue().score());
         });
 
         // Method 3:
-        Map<String, Student> mapTopStudent2 = students.stream().collect(Collectors.toMap(Student::getSubject, s -> s, (s1, s2) -> s1.getScore() > s2.getScore()? s1 : s2));
+        Map<String, Student> mapTopStudent2 = students.stream().collect(Collectors.toMap(Student::subject, s -> s, (s1, s2) -> s1.score() > s2.score()? s1 : s2));
         mapTopStudent2.entrySet().forEach(x -> {
-            log.info("Method 3: Subject: {} | Student: {} | Score: {}", x.getKey(), x.getValue().getStudentName(), x.getValue().getScore());
+            log.info("Method 3: Subject: {} | Student: {} | Score: {}", x.getKey(), x.getValue().studentName(), x.getValue().score());
         });
     }
 }
