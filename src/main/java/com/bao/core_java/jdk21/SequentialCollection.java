9ccfd6589e8c01b196e318e1759f1ce165306def
baonguyen5759 <quocnb@gmail.com>
2026-08-18 15:19:10 -0500

    stream

--- src/main/java/com/bao/core_java/jdk21/SequentialCollection.java
+++ src/main/java/com/bao/core_java/jdk21/SequentialCollection.java
@@ -2,7 +2,9 @@ package com.bao.core_java.jdk21;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 @Slf4j
@@ -12,5 +14,10 @@ public class SequentialCollection {
         emps.add("B*** Ng****");
         emps.add("J*** R*****");
         log.info("Last employee: {}", emps.getLast());
+
+        Deque<String> deque = new ArrayDeque<>();
+        deque.add("JDK 17");
+        deque.add("JDK 21");
+        log.info("Front: {} | Back: {}", deque.getFirst(), deque.getLast());
     }
 }
--- src/main/java/com/bao/core_java/stream/Sorting.java
+++ src/main/java/com/bao/core_java/stream/Sorting.java
@@ -4,31 +4,42 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
 
 @Slf4j
 public class Sorting {
+    @Data
+    @AllArgsConstructor
+    class Account {
+        private String name;
+        private int balance;
+    }
 
     void main() {
-        @Data
-        @AllArgsConstructor
-        class Account {
-            private String name;
-            private int balance;
-        }
 
         List<Account> accounts = List.of(new Account("A1", 10), new Account("A4", 5), new Account("A3", 30), new Account("A2", 5));
 
         List<Account> sortedAccounts = accounts.stream().sorted(Comparator.comparing(Account::getBalance).thenComparing(Account::getName)).toList();
         log.info("Result 1: {}", sortedAccounts);
 
-        Function<Account, String> byName = e -> e.getName();
-
+        Function<Account, String> byName = Account::getName;
         Function<Account, Integer> byBalance = Account::getBalance;
+
         List<Account> sortedAccounts2 = accounts.stream().sorted(Comparator.comparing(byBalance).thenComparing(byName)).toList();
         log.info("Result 2: {}", sortedAccounts2);
+
+        Integer[] array = {5,1,9,2};
+        Arrays.sort(array, Comparator.reverseOrder());
+        log.info("After sorting 1: {}", Arrays.toString(array));
+
+        int[] a = {5,1,9,2};
+        Integer[] b = Arrays.stream(a).boxed().toList().toArray(Integer[]::new);
+        Arrays.sort(b, Comparator.reverseOrder());
+        log.info("After sorting 2: {}", Arrays.toString(b));
+
+        int[] bb = Arrays.stream(a).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray();
+        log.info("After sorting 3: {}", bb);
+
     }
 }
